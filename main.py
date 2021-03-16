9dbc7fdb1df90a90c1a21935421bc65d4a69ca55
Isaaafc <isaaafc@gmail.com>
2021-03-16 16:03:49 +0800

    Filter na cols

--- main.py
+++ main.py
@@ -9,5 +9,5 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    ui = UI(args.corpus_path, args.tokens_path, config['Save'], config['UI']['options'], config['Input']['tokens_cols'], config['UI']['display_range'], config['Input']['filter_na_cols'])
+    ui = UI(args.corpus_path, args.tokens_path, config['Save'], config['UI']['options'], config['Input']['tokens_cols'], config['UI']['display_range'], filter_na_cols=config['Input']['filter_na_cols'])
     ui.start()
